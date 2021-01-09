70a5bf2e112bca4c7af2cd77b553add6582e77a4
Eshan2009 <72752337+Eshan2009@users.noreply.github.com>
2021-01-09 10:31:45 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var database;
+var form,game,player;
+var gameState = 0
+var playerCount = 0
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(800,800);
+
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
+
